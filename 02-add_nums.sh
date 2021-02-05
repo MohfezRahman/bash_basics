b413fb02d71487d175a1d4b579876e5912f87220
Mohfez Rahman <mrahman5@ggc.edu>
2021-02-05 02:02:03 -0500

    Made change on 02-add_nums.sh file

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -9,7 +9,7 @@ read third
 
 sum=$(( first+second+third ))
 
-echo "The sum  is $sum. Thank you for your time."
+echo "The sum  is $sum."
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
